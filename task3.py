ef7db15fdbfb342ee0d6a35cdab383a0924177dc
Yuliia Krasnokutska <yuliia.krasnokutska.cr@gmail.com>
2024-10-06 21:14:55 +0300

    task 3 done

--- task3.py
+++ task3.py
@@ -1,19 +1,23 @@
 import re
 
+
 def normalize_phone(phone_number):
-    pattern = r"+38.+ \d+"
-    match = re.search(pattern, phone_number)
-    if match:
-        print("match: ", match.group())
-
-    #clean = phone_number.strip()
-    #print(clean)
-
-    
-    # if phone_number.startswith('+'):
-    #     pass
-    # else: phone_number = f"+ {phone_number}"
-
+    # The function formats numbers into the correct form.
+    # The function returns the formatted phone number (list item) and is executed as many times 
+    # as there are items in the list.
+    # Finds the “+” character and numbers in the string and deletes other symbols.
+    phone_number = re.sub(r"[^\+\d]", "", phone_number)
+    # Checks for country code (38).
+    if phone_number.startswith("38"):
+        phone_number = "+" + phone_number
+        return phone_number
+    # Checks for the presence of the “+” character.
+    elif phone_number.startswith("+"):
+        return phone_number
+    # Otherwise adds the “+” character and the country code.
+    else:
+        phone_number = "+38" + phone_number
+        return phone_number
 
 
 raw_numbers = [
